bee779cfe5c6c039aa53bb0f30d7baf72ab10538
bhague1281 <bhague1281@gmail.com>
2016-03-21 16:11:57 -0700

    gulp sass example

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
@@ -15,4 +16,10 @@ gulp.task('build', function() {
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('sass', function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['sass', 'build']);
